d54237ad9824158105e2fea51bafcafa2e52a672
Koblížek3 <janprokupek@centrum.cz>
2022-03-11 16:11:19 +0100

    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # JCommands
-Test project
+Java library for easier use of JDA
+
+### Installation
+Checkout releases and download the latest jar with all required dependencies.  
+### Usage
+```java
+class Main {
+    public static void main(String[] args) {
+        JDA jda = JDABuilder.createDefault("Your token").build();
+        JApi.init(jda, "your prefix", true);
+        
+        new Command("commandName", (event) -> {
+            // Action that will run once command is called 
+        });
+    }
+}
+```
+### Planned features
+1) Slash commands - in work but not yet released
+2) Better event listeners
+3) Buttons
+4) Better embeds
+##### Contacts
+> GamestationCZ#4663
--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -1,7 +1,10 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.jcommands.JApi;
 import net.jcommands.commands.Command;
+import net.jcommands.commands.SlashCommand;
 import net.jcommands.exceptions.JApiException;
 
 import javax.security.auth.login.LoginException;
@@ -16,5 +19,11 @@ public class Start {
         new Command("build", (event -> {
             // Whatever
         }));
+        new SlashCommand("ping", "test command", new OptionData[] {
+            new OptionData(OptionType.STRING, "NameOfOption", "Description of option"),
+            new OptionData(OptionType.MENTIONABLE, "NameOfOption2", "Description of option2"),
+        }, (event) -> {
+            // Do this whenever someone interacts with "ping" command
+        });
     }
 }
